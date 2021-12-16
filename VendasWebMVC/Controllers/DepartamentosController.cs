5817e28eda607675e56efeff86d233ac44a0d90b
Erlan79 <erlanlimasantos@gmail.com>
2021-12-16 20:11:55 -0300

    Primeira Visão de Controlador de Modelo - Departamento

---
+++ VendasWebMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Models;
+
+namespace VendasWebMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Nome = "Eletrônico" });
+            list.Add(new Departamento { Id = 2, Nome = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
